9687fadaf0cae6872669ca52f0c5d0d62279cfee
Marie Lowzow Morland <marie@morland.no>
2021-05-06 19:19:09 +0200

    -about us page

--- js/pages/about.js
+++ js/pages/about.js
@@ -3,3 +3,8 @@ import addFooterForPage from "../templates/footer.js";
 
 addHeaderForPage(pageNames.ABOUT);
 addFooterForPage();
+
+const insertCorrectAge = () => {
+  document.getElementById("myage").innerHTML = new Date().getFullYear() - 1 - 1989;
+}
+insertCorrectAge();
