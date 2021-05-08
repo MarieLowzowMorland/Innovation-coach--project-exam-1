8314b9abe9172ad367222f198c42db4be9098031
Marie Lowzow Morland <marie@morland.no>
2021-05-08 12:21:46 +0200

    -Added hotjar

--- js/templates/header.js
+++ js/templates/header.js
@@ -1,29 +1,29 @@
-import { HamburgerMenu } from '../templates/svgIcons.js';
+import { HamburgerMenu } from "../templates/svgIcons.js";
 
 export const pageNames = {
   HOME: {
     name: "Home",
-    url: "index.html"
+    url: "index.html",
   },
   ALL_POSTS: {
     name: "All Posts",
-    url: "allPosts.html"
+    url: "allPosts.html",
   },
   POST: {
     name: "Post",
-    url: "post.html"
+    url: "post.html",
   },
   TOOLS_AND_LINKS: {
     name: "Tools and Links",
-    url: "toolsAndLinks.html"
+    url: "toolsAndLinks.html",
   },
   ABOUT: {
     name: "About",
-    url: "about.html"
+    url: "about.html",
   },
   CONTACT: {
     name: "Contact",
-    url: "contact.html"
+    url: "contact.html",
   },
 };
 
@@ -33,40 +33,41 @@ const stickyNav = () => {
     const topOfNavbar = nav.offsetTop;
 
     if (window.pageYOffset > topOfNavbar) {
-      nav.classList.add("stick-to-top")
+      nav.classList.add("stick-to-top");
     } else {
       nav.classList.remove("stick-to-top");
     }
   }
 
   window.onscroll = checkIfNavbarShouldStickToTop;
-}
+};
 
 const toggleMenu = (event) => {
   event.stopPropagation();
   event.currentTarget.blur();
   document.getElementById("menu").classList.toggle("open-menu");
-}
+};
 
-const addHeaderForPage = (selectedPage) => {  
+const addHeaderForPage = (selectedPage) => {
   document
     .querySelector("main")
     .insertAdjacentHTML("beforebegin", headerTemplate(selectedPage));
 
   document.getElementById("menuline").addEventListener("click", toggleMenu);
   stickyNav();
-}
+};
 
-const link = ( selectedPage, linkToPage ) =>  /*template*/ `
+const link = (selectedPage, linkToPage) => /*template*/ `
   <li>
-    <a href="${linkToPage.url}" data-text="${linkToPage.name}" class="${selectedPage === linkToPage ? "active" : ""} discrete-button">
+    <a href="${linkToPage.url}" data-text="${linkToPage.name}" class="${
+  selectedPage === linkToPage ? "active" : ""
+} discrete-button">
       ${linkToPage.name}
     </a>
     <span class="circle"></span>
   </li>`;
-  
 
-const headerTemplate = ( selectedPage ) => {
+const headerTemplate = (selectedPage) => {
   const isHomePage = selectedPage === pageNames.HOME;
   const nameTagMobile = isHomePage ? "h1" : "span";
   const nameTagTablet = isHomePage ? "h1" : "p";
@@ -75,7 +76,9 @@ const headerTemplate = ( selectedPage ) => {
     <nav>
       <div id="menu">
         <button id="menuline" class="tablet-hidden">
-          <${nameTagMobile} class="pagename">${selectedPage.name}</${nameTagMobile}>
+          <${nameTagMobile} class="pagename">${
+    selectedPage.name
+  }</${nameTagMobile}>
           <span id="hamburger-menu">${HamburgerMenu()}</span>
         </button>
         <ul>
@@ -94,8 +97,20 @@ const headerTemplate = ( selectedPage ) => {
       </div>
       <img src="images/Hovedbilde.jpg" height="200" class="header-image" alt=""></div>
     </div>
+
+    <!-- Hotjar Tracking Code for https://innovation-coach.netlify.app/ -->
+    <script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2390574,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+    
   </header>`;
-}
-      
+};
 
 export default addHeaderForPage;
