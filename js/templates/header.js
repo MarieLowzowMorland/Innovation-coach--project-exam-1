97568175f6758a006fc0b55cb1db56506de05f70
Marie Lowzow Morland <marie@morland.no>
2021-05-08 13:17:42 +0200

    ups.... hotjar is now right.

--- js/templates/header.js
+++ js/templates/header.js
@@ -95,20 +95,6 @@ const headerTemplate = (selectedPage) => {
         <a class="cta" href="allPosts.html"> See newest articles </a>
       </div>
       <img src="images/Hovedbilde.jpg" height="200" class="header-image" alt=""></div>
-    </div>
-
-    <!-- Hotjar Tracking Code for https://innovation-coach.netlify.app/ -->
-    <script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:2390574,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
-    
   </header>`;
 };
 
