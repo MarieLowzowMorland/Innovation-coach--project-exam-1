a059f8a47b5f77e43fac3a733d8c097fcd647bb2
Marie Lowzow Morland <marie@morland.no>
2021-05-19 12:18:37 +0200

    - made the links on All-posts smaller so that one does not have to read everything on the page with screen reader

--- js/components/post.js
+++ js/components/post.js
@@ -3,21 +3,21 @@ const postToHtml = (post) => {
 
   return /*template*/`
     <div class="post-wrapper">
-      <a href="post.html?id=${id}" class="post">
+      <div class="post">
         <div>
           <div class="reverse-column-order">
             <h2>${title}</h2>
             <p class="date">${dateString}</p>
           </div>
           <div class="article-introduction">${summary}</div>
-          <p class="link">Read more </p>
+          <a href="post.html?id=${id}" aria-label="Read more about ${title}" class="link">Read more </a>
         </div>
         <!-- No alt text for posts in a "list" context -->
         <div role="img" 
           class="post-image-wrapper" 
           style='background-image: url("${featuredImage.src}")'>
         </div>
-      </a>
+      </div>
     </div>`;
 };
 
