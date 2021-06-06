d754f1e4fcf6bdb08e9754ef5ced432210b5117a
Marie Lowzow Morland <marie@morland.no>
2021-06-06 14:46:28 +0200

    -loader template had quotes around the element tag

--- js/templates/loader.js
+++ js/templates/loader.js
@@ -1,9 +1,9 @@
 export const Loader = () => /*template*/
-  `"<div id="loader" role="img" aria-label="Loading page.">
+  `<div id="loader" role="img" aria-label="Loading page.">
   <span id="fisrt-circle"></span>
   <div id="line-wrapper"><span id="line"></span></div>
   <span id="second-circle"></span>
-  </div>"`;
+  </div>`;
 
 export const removeLoader = () =>
 document.getElementById("loader").remove();
